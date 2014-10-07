f0bd21f2596541dc4aade73ad562848ca78f2b37
Pietro Martinelli <amicofragile@gmail.com>
2014-10-07 13:59:41 +0200

    Program should print "Buzz" instead of the number for multiples of five Program should print "FizzBuzz" instead of the number for multiples of both three and five

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -3,8 +3,12 @@ public class FizzBuzz {
 	public String toString() {
 		final StringBuilder sb = new StringBuilder();
 		for (int i = 1; i <= 100; i++) {
-			if(isMultipleOfThree(i)) {
+			if(isMultipleOfBothThreeAndFive(i)) {
+				sb.append("FizzBuzz");
+			} else if(isMultipleOfThree(i)) {
 				sb.append("Fizz");
+			} else if(isMultipleOfFive(i)) {
+				sb.append("Buzz");
 			} else {
 				sb.append(i);
 			}
@@ -13,7 +17,15 @@ public class FizzBuzz {
 		return sb.toString();
 	}
 
+	private boolean isMultipleOfBothThreeAndFive(int i) {
+		return isMultipleOfThree(i) && isMultipleOfFive(i);
+	}
+
 	private boolean isMultipleOfThree(int i) {
 		return i % 3 == 0;
+	}
+	
+	private boolean isMultipleOfFive(int i) {
+		return i % 5 == 0;
 	}
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -9,18 +9,40 @@ public class FizzBuzzTest {
 		final String[] items = result.split(" ");
 		assertEquals(100, items.length);
 		for (int i = 1; i <= 100; i++) {
-			if(i % 3 != 0) {
+			if(i % 3 != 0 && i % 5 != 0) {
 				assertEquals(Integer.toString(i), items[i - 1]);
 			}
 		}
 	}
 
 	@Test
-	public void multipleOfThreeAreSubstitutedByFuzz() throws Exception {
+	public void multipleOfThreeAreSubstitutedByFizz() throws Exception {
 		final String result = new FizzBuzz().toString();
 		final String[] items = result.split(" ");
 		for (int i = 3; i <= 100; i += 3) {
-			assertEquals("Fizz", items[i - 1]);
+			if(i % 3 != 0) {
+				assertEquals("Fizz", items[i - 1]);
+			}
+		}
+	}
+	
+	@Test
+	public void multipleOfFiveAreSubstitutedByBuzz() throws Exception {
+		final String result = new FizzBuzz().toString();
+		final String[] items = result.split(" ");
+		for (int i = 5; i <= 100; i += 5) {
+			if(i % 3 != 0) {
+				assertEquals("Buzz", items[i - 1]);
+			}
+		}
+	}
+
+	@Test
+	public void multipleOfThreeAndFiveAreSubstitutedByFizzBuzz() throws Exception {
+		final String result = new FizzBuzz().toString();
+		final String[] items = result.split(" ");
+		for (int i = 15; i <= 100; i += 15) {
+			assertEquals("FizzBuzz", items[i - 1]);
 		}
 	}
 }
